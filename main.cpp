c7c0b0a6330b97b1e6df481e7a523187ac7e46d4
racha4 <rachakheiredine@gmail.com>
2026-05-31 23:28:10 +0100

    swapping 3 numbers

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 int main()
 {
-    cout<<"          1\n";
-    cout<<"        1   1\n";
-    cout<<"      1   2   1\n";
-    cout<<"    1   3   3   1\n";
-    cout<<"  1   4   6   4   1\n";
-    cout<<"1   5   10   10   5  1\n";
+    int a , b , c ,x ;
+    cin >> a >> b >> c ;
+    x=a;
+    a=b;
+    b=c;
+    c=x;
+    cout << a << " " << b << " " << c ;
+
 }
