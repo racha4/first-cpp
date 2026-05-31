88c60a0b60c2d26a84e3259fb15560ba4c2a693e
racha4 <rachakheiredine@gmail.com>
2026-05-31 23:44:59 +0100

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a , b , c ,x ;
+    cin >> a >> b >> c ;
+    x=a;
+    a=b;
+    b=c;
+    c=x;
+    cout << a << " " << b << " " << c ;
+
+}
